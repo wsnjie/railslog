194ca84e853feea208cc99c19cdffa7cf4a29c08
Will <megapocketninja@gmail.com>
2019-04-25 10:40:48 -0400

    Added comments, delete for comments.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    has_many :comments
     validates :title, presence: true,
                     length: { minimum: 7 }
+    validates :text, presence: true
 end
